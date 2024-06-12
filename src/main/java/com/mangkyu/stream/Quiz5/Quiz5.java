cfe39aaebfb9bd0b2b337abfb7c395b96c393bc2
juhwan <kjhw990218@gmail.com>
2024-06-12 23:41:12 +0900

    feat: Quiz4,5 풀이추가 - Quiz4 7번은 아직 푸는 중 - Quiz5 3,4번 아직 푸는 중

--- src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
+++ src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
@@ -1,7 +1,6 @@
 package com.mangkyu.stream.Quiz5;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Quiz5 {
 
@@ -9,17 +8,23 @@ public class Quiz5 {
 
     // 5.1 모든 문자열의 길이를 더한 결과를 출력하여라.
     public int quiz1() {
-        return 0;
+        return Arrays.stream(STRING_ARR)
+                .mapToInt(String::length)
+                .sum();
     }
 
     // 5.2 문자열 중에서 가장 긴 것의 길이를 출력하시오.
     public int quiz2() {
-        return 0;
+        return Arrays.stream(STRING_ARR)
+                .mapToInt(String::length)
+                .max()
+                .orElse(0);
     }
 
     // 5.3 임의의 로또번호(1~45)를 정렬해서 출력하시오.
     public List<Integer> quiz3() {
-        return Collections.emptyList();
+
+        return null;
     }
 
     // 5.4 두 개의 주사위를 굴려서 나온 눈의 합이 6인 경우를 모두 출력하시오.
