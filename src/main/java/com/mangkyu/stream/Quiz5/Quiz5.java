feb000d33274302ee6b7e3787fb5bb00188af883
juhwan <kjhw990218@gmail.com>
2024-06-13 13:04:43 +0900

    feat: Quiz5 3번 풀이 추가

--- src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
+++ src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
@@ -1,6 +1,7 @@
 package com.mangkyu.stream.Quiz5;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Quiz5 {
 
@@ -23,8 +24,12 @@ public class Quiz5 {
 
     // 5.3 임의의 로또번호(1~45)를 정렬해서 출력하시오.
     public List<Integer> quiz3() {
-
-        return null;
+        return new Random().ints(1, 46)
+                .distinct()
+                .limit(6)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     // 5.4 두 개의 주사위를 굴려서 나온 눈의 합이 6인 경우를 모두 출력하시오.
